8c631a1f8e7e746f2cfc3d2060998bda93355c9f
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 11:32:13 -0600

    add if else if else statement

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening.");
+            }
         }
     }
 }
