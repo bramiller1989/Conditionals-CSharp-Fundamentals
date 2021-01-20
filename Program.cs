d53060d81a049fb58a857fe8fab30d28bf40233b
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 11:40:36 -0600

    extract enum into own file

--- Program.cs
+++ Program.cs
@@ -2,14 +2,6 @@
 
 namespace Conditionals
 {
-    public enum Season
-    {
-        Spring,
-        Summer,
-        Autumn,
-        Winter
-    }
-
     class Program
     {
         static void Main(string[] args)
